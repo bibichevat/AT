9f8c23d7a1ba819b6a05a0e44cef36e2e533766c
bibichevat <36561119+bibichevat@users.noreply.github.com>
2025-03-26 16:10:23 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // массив строк и чисел для проверки
         ArrayList<Object> list = new ArrayList<>(Arrays.asList("а роза упала на лапу Азора", "1991", "Don't nod",
-                "No lemon, no melon", "Do it, Tido", 12512, 112121, "привет"));
+                "No lemon, no melon", "Do it, Tido", 12512, 112121, "привет", "ЁЁЁ"));
         for (Object text: list) {
             if (isPalindrome(String.valueOf(text))) {
                 System.out.println(text + " - palindrome");
@@ -21,8 +21,9 @@ public class Main {
            удаление не буквенно-числовых значений
            приведение строки к нижнему регистру
         */
-        text = text.replaceAll("[^a-zA-Zа-яА-Я0-9]", "").toLowerCase();
+        text = text.replaceAll("[^a-zA-Zа-яА-Я0-9ёЁ]", "").toLowerCase();
         // посимвольное сравнение левой и правой части строки
+        System.out.println(text);
         for (int i = 0; i < text.length() / 2; i++) {
             // если пара символов не совпадает, строка не является палиндромом
             if (text.charAt(i) != text.charAt(text.length()-1-i)) {
